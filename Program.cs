c4730a99b6ab4ce064996b8c1c5f2716481e3cc0
Alan da Costa Santos <alancostasantos07@gmail.com>
2020-11-16 14:18:29 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Calculadora_B_sica_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
