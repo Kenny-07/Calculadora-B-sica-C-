60991a8bab624612a7247c319c133b27e8046489
Alan da Costa Santos <alancostasantos07@gmail.com>
2020-11-16 15:58:03 -0300

    Calculadora Finalizada

--- Program.cs
+++ Program.cs
@@ -6,7 +6,41 @@ namespace Calculadora_B_sica_C_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bem vindo a calculadora mágica, digite qual a operação deseja fazer ?? ( + , - , * , / )");
+            string operacao = Console.ReadLine();
+
+            Console.WriteLine("Digite o 1º valor: ");
+            float num1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o 2º valor: ");
+            float num2 = float.Parse(Console.ReadLine());
+
+            float resultado = 0f;
+
+            switch(operacao){
+                case "+":
+                    resultado = num1 + num2;
+                break;
+
+                case "-":
+                    resultado = num1 - num2;
+                break;
+
+                case "*":
+                    resultado = num1 * num2;
+                break;
+
+                case "/":
+                    resultado = num1 / num2;
+                break;
+
+                default:
+                    operacao = "invalida";
+                    Console.WriteLine("Operação Inválida :c");
+                break;
+            }
+
+            Console.WriteLine($"Calculo: {num1} com {num2} = {resultado}");
         }
     }
 }
